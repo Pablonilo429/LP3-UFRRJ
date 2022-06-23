4732637384b7908313bc28615d8adde9b7e8faca
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2022-06-23 18:51:18 -0300

    Exercicio usando vetor e classe

--- Aula 23-06-2022/Aula23062022teste.java
+++ Aula 23-06-2022/Aula23062022teste.java
@@ -1,8 +1,26 @@
 public class Aula23062022teste {
     public static void main(String[] args) {
-        Aula23062022 funcionario = new Aula23062022("Pablo de Oliveira", 10000.0, 10);
+        Aula23062022[] funcionario = new Aula23062022[5];
 
-        double salario_total = funcionario.calcular_salario();
-        System.out.printf("O salario total = %.2f", salario_total);
+        funcionario[0] = new Aula23062022("Joyce", 8000.0, 3);
+        funcionario[1] = new Aula23062022("Patrick", 9500.0, 4);
+        funcionario[2] = new Aula23062022("Benvindo", 11000.0, 2);
+        funcionario[3] = new Aula23062022("Abhner", 15000.0, 3);
+        funcionario[4] = new Aula23062022("Nicoli", 12500.0, 5);
+
+        String nome;
+        double salario_total;
+        int i;
+
+        for(i = 0; i < 5; i++){
+            nome = funcionario[i].getNome();
+            salario_total = funcionario[i].calcular_salario();
+
+            System.out.printf("Funcionario %s Salario: %.2f \n", nome, salario_total);
+        }
+
+
+
+
     }
 }
