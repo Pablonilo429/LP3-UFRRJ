a81511472bf69ffa50b44d7d90d1cd65463fbe65
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2022-07-30 17:14:52 -0300

    Segunda opção do mesmo codigo
    Co-Authored-By: Helon de França <92336676+helonfranca@users.noreply.github.com>
    Co-Authored-By: Vanessa Andrade <82402822+vandradesa@users.noreply.github.com>

--- PrimeiroTrabalhoLP3/src/Exercicio4/TesteRetangulo.java
+++ PrimeiroTrabalhoLP3/src/Exercicio4/TesteRetangulo.java
@@ -7,6 +7,26 @@ public class TesteRetangulo {
         Retangulo[] retangulo = new Retangulo[2];
         Scanner input = new Scanner(System.in);
 
+        /*Opcao com mais linhas de codigo
+
+        Retangulo retangulo1 = new Retangulo;
+        Retangulo retangulo2 = new Retangulo;
+        System.out.printf("Entre com o comprimento do retangulo 1\n");
+        retangulo1.setComprimento(Double.parseDouble(input.nextLine()));
+        System.out.printf("Entre com a largura do retangulo 1\n");
+        retangulo1.setLargura(Double.parseDouble(input.nextLine()));
+        System.out.printf("O perimetro do retangulo1: %.1f\n",retangulo1.perimetro());
+        System.out.printf("A area do retangulo1: %.1f\n",retangulo1.area());
+        System.out.printf("Entre com o comprimento do retangulo 2\n");
+        retangulo2.setComprimento(Double.parseDouble(input.nextLine()));
+        System.out.printf("Entre com a largura do retangulo 2\n");
+        retangulo2.setLargura(Double.parseDouble(input.nextLine()));
+        System.out.printf("O perimetro do retangulo1: %.1f\n",retangulo2.perimetro());
+        System.out.printf("A area do retangulo1: %.1f\n",retangulo2.area());
+        */
+
+        //Opcao com menos codigo
+
         for(int i = 0 ; i < 2;i++){
             retangulo[i] = new Retangulo();
             System.out.printf("Entre com o comprimento do retangulo %d \n",i+1);
